278a101dd33a189fbeb950c67073adea03458d37
ng-minhdat <nmdat21@clc.fitus.edu.vn>
2023-05-17 20:38:57 +0700

    [UPDATE] hello world

--- first.js
+++ first.js
@@ -1,1 +1,16 @@
 console.log("This is my first file to learn Node.js")
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/surprise', (req, res) => {
+    res.send('I am the man behind this page!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
